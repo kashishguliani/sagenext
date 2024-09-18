c4192ffb8f160ffbf60242a2e1a1d2c13605b9a2
kashishguliani <126547582+kashishguliani@users.noreply.github.com>
2024-09-18 18:57:42 +0530

    Add files via upload

---
+++ Page2.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import "./Page2.scss"
+
+const Page2 = () => {
+  return (
+    <div className='container'>
+        <div className='box-2'>
+            <div className='top'>
+                Why SageNest?
+            </div>
+            <div className='c1'>
+                Revolutionising Personalised Learning for Everyone
+            </div>
+            <div className='p1'>
+                SageNest empowers individuals by providing personalized learning experiences tailored to their unique needs, goals, and schedules. Whether you are an expert looking to share your knowledge or a learner seeking to acquire new skills, SageNest is your gateway to a world of limitless possibilities.
+            </div>
+            <div className='p2'>
+                In a world where one-size-fits-all solutions are no longer effective, SageNest offers a platiorm designed to cater to the individual. We understand that everyone's learning journey is different, which is why our platform is built on the principles of personalization, flexibility,and connection.
+            </div>
+            <div className='dash'>
+                <span>----</span>
+            </div>
+            <div className='h2'>
+                How SageNest Improves Lives
+            </div>
+            <div className='box-con'>
+                <div className='boxi'>
+                    <div className='up'>
+                        <div className='up-left'>logo</div>
+                        <div className='up-right'>1</div>
+                    </div>
+                    <div className='mid'>
+                        Heading 1
+                    </div>
+                    <div className='down'>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                    </div>
+                </div>
+                <div className='boxi'>
+                    <div className='up'>
+                        <div className='up-left'>logo</div>
+                        <div className='up-right'>2</div>
+                    </div>
+                    <div className='mid'>
+                        Heading 1
+                    </div>
+                    <div className='down'>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                    </div>
+                </div>
+                <div className='boxi'>
+                    <div className='up'>
+                        <div className='up-left'>logo</div>
+                        <div className='up-right'>3</div>
+                    </div>
+                    <div className='mid'>
+                        Heading 1
+                    </div>
+                    <div className='down'>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Page2
