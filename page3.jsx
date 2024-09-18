bdd9fd521669f8b53d37d7a60cf6334ac4fea018
kashishguliani <126547582+kashishguliani@users.noreply.github.com>
2024-09-18 18:56:55 +0530

    Add files via upload

---
+++ page3.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import './Page3.scss';
+
+const Page3 = () => {
+  return (
+    <div className='container'>
+      <div className='box-2'>
+        <div className='top'>
+          For Experts
+        </div>
+        <div className='c1'>
+          Evaluate your complex and grow your influence
+        </div>
+        <div className='p1'>
+          Share expertise across borders, offering guidance of a global scale
+        </div>
+        <div className='dash'>
+          <span>----</span>
+        </div>
+        <div className='box-con'>
+          <div className='boxi'>
+            <div className='up'>
+              <div className='up-left'>logo</div>
+              <div className='up-right'>1</div>
+            </div>
+            <div className='mid'>
+              Heading 1
+            </div>
+            <div className='down'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+            </div>
+          </div>
+          <div className='boxi'>
+            <div className='up'>
+              <div className='up-left'>logo</div>
+              <div className='up-right'>2</div>
+            </div>
+            <div className='mid'>
+              Heading 1
+            </div>
+            <div className='down'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+            </div>
+          </div>
+          <div className='boxi'>
+            <div className='up'>
+              <div className='up-left'>logo</div>
+              <div className='up-right'>3</div>
+            </div>
+            <div className='mid'>
+              Heading 1
+            </div>
+            <div className='down'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page3;
